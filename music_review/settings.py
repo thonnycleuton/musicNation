302de44f27e7ee8206190a5e981d203170b65a95
thonnycleuton <thonnycleuton@gmail.com>
2018-07-04 14:38:49 -0300

    migration files

--- music_review/settings.py
+++ music_review/settings.py
@@ -127,4 +127,13 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '2/hour',
+        'user': '5/hour',
+        'api-token': '1/hour',
+    },
 }
